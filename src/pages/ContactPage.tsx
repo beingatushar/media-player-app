47901169d9c92abe30ab47197064a457d18aa970
Tushar Aggarwal <beingatushar@gmail.com>
2024-05-17 23:03:10 +0530

    fixed overflow in contacts page

--- src/pages/ContactPage.tsx
+++ src/pages/ContactPage.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import {
     AiFillInstagram,
     AiFillLinkedin,
@@ -7,26 +8,26 @@ import {
 
 const ContactPage: React.FC = () => {
     return (
-        <div className="min-h-screen  text-white py-12 px-4 col-span-3">
+        <div className="min-h-screen text-white py-12 px-4 col-span-3">
             <div className="container mx-auto max-w-4xl">
                 <h1 className="text-4xl font-bold mb-8 text-center text-white">
                     Let's Connect
                 </h1>
                 <div className="bg-white bg-opacity-10 backdrop-filter backdrop-blur-lg rounded-lg p-8 shadow-lg flex justify-center">
-                    <div className="grid grid-cols-1 md:grid-cols-2 gap-8 text-xl">
+                    <div className="grid grid-cols-1 md:grid-cols-2 gap-8 text-md lg:text-xl">
                         <div className="flex flex-col items-start">
                             <div className="mb-6 flex items-center">
-                                <AiOutlineMail className="mr-2 " />
-                                <strong className="text-lg">Email:</strong>
+                                <AiOutlineMail className="mr-2" />
+                                <strong className="">Email:</strong>
                                 <a
                                     href="mailto:aggarwaltushar36@gmail.com"
-                                    className="ml-2  underline"
+                                    className="ml-2 underline break-all"
                                 >
                                     aggarwaltushar36@gmail.com
                                 </a>
                             </div>
                             <div className="mb-6 flex items-center ">
-                                <AiFillPhone className="mr-2 " />
+                                <AiFillPhone className="mr-2" />
                                 <strong className="">Phone:</strong>
                                 <span className="ml-2">+91 85868 10252</span>
                             </div>
@@ -37,7 +38,7 @@ const ContactPage: React.FC = () => {
                                 <strong className="">
                                     <a
                                         href="https://www.linkedin.com/in/beingatushar"
-                                        className=" hover:text-gray-100 ml-2 "
+                                        className="hover:text-gray-100 ml-2"
                                         target="_blank"
                                         rel="noopener noreferrer"
                                     >
@@ -47,10 +48,10 @@ const ContactPage: React.FC = () => {
                             </div>
                             <div className="mb-6 flex items-center">
                                 <AiFillInstagram className="mr-2 text-red-300" />
-                                <strong className="text-lg ">
+                                <strong className="lg:text-lg">
                                     <a
                                         href="https://www.instagram.com/beingatushar"
-                                        className=" hover:text-gray-100 ml-2  "
+                                        className="hover:text-gray-100 ml-2"
                                         target="_blank"
                                         rel="noopener noreferrer"
                                     >
